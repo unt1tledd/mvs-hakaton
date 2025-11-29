25cb4d471aad0d51a83a6981630f858bc0e132ae
unt1tledd <sonyaskin@icloud.com>
2025-11-29 16:34:49 +0300

    upd config

--- app/config.py
+++ app/config.py
@@ -3,6 +3,17 @@ import os
 
 load_dotenv()
 
-TOKEN = os.getenv("MTS_API")
-if not TOKEN:
-    raise RuntimeError("Add token")
+MWS_TOKEN = os.getenv("MTS_API")
+VIEW_ALL = os.getenv("VIEW_ALL")
+DST_ALL = os.getenv("DST_VK")
+
+VK_TOKEN = os.getenv("VK_TOKEN")
+VIEW_VK = os.getenv("VIEW_VK")
+DST_VK = os.getenv("DST_VK")
+
+TG_TOKEN = os.getenv("TG_TOKEN")
+VIEW_TG = os.getenv("VIEW_TG")
+DST_TG = os.getenv("DST_TG")
+
+YANDEX_METRIKA_TOKEN = os.getenv("YANDEX_METRIKA_TOKEN")
+VIEW_YANDEX_METRIKA = os.getenv("VIEW_YANDEX_METRIKA")
